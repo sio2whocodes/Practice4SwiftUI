a0fde2f89b29184afc38f5964462025bf9214cd0
sio2whocodes <fltcy2039@naver.com>
2024-07-01 22:43:00 +0900

    Initial Commit

---
+++ Practice4SwiftUI/Practice4SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice4SwiftUIApp.swift
+//  Practice4SwiftUI
+//
+//  Created by sujeong on 2024/07/01.
+//
+
+import SwiftUI
+
+@main
+struct Practice4SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
