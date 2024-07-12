0f0853f9d7977f69af0a2cebae187c97c1cbc754
sio2whocodes <fltcy2039@naver.com>
2024-07-12 13:32:30 +0900

    slots app challenges code update

--- Slots Demo/Slots Demo/ContentView.swift
+++ Slots Demo/Slots Demo/ContentView.swift
@@ -1,4 +1,4 @@
- //
+//
 //  ContentView.swift
 //  Slots Demo
 //
@@ -27,7 +27,7 @@ struct ContentView: View {
             
             VStack {
                 Spacer()
-
+                
                 // Title
                 HStack {
                     Image(systemName: "moon.stars.circle.fill").foregroundColor(.yellow)
@@ -37,7 +37,7 @@ struct ContentView: View {
                     Image(systemName: "moon.stars.circle.fill").foregroundColor(.yellow)
                 }.scaleEffect(2)
                 Spacer()
-
+                
                 // Credits counter
                 Text("Credits: " + String(credits))
                     .foregroundColor(.black)
@@ -45,7 +45,7 @@ struct ContentView: View {
                     .background(Color.white.opacity(0.5))
                     .cornerRadius(20)
                 Spacer()
-
+                
                 // Cards
                 HStack {
                     Spacer()
@@ -53,7 +53,7 @@ struct ContentView: View {
                     CardView(symbol: $symbols[number[0]], background: $backgrounds[0])
                     CardView(symbol: $symbols[number[1]], background: $backgrounds[1])
                     CardView(symbol: $symbols[number[2]], background: $backgrounds[2])
-
+                    
                     Spacer()
                 }
                 HStack {
@@ -72,79 +72,188 @@ struct ContentView: View {
                 }
                 
                 Spacer()
-
+                
                 // First Button
-                HStack {
-                    Button {
-                        // Background Color Initiate
-                        backgrounds = backgrounds.map { _ in Color.white }
-                        
-                        // Change the images
-                        number = number.map{ _ in
-                            Int.random(in: 0..<symbols.count)
+                HStack(spacing: 20.0) {
+                    VStack {
+                        Button {
+                            processResults(false)
+                        } label: {
+                            Text("Spin")
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .padding(.horizontal, 30)
+                                .background(Color.pink)
+                                .cornerRadius(10)
                         }
-                        
-                        // Check winnings
-                        // middle row
-                        if number[3] == number[4] && number[4] == number[5] {
-                            
-                            // Won
-                            credits += betAmount * 10
-                            
-                            // Update backgrounds to green
-                            backgrounds = backgrounds.map{ _ in Color.green }
-                            
-                        } else {
-                            credits -= betAmount
-                        }
-                        
-                    } label: {
-                        Text("Spin")
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .padding(.horizontal, 30)
-                            .background(Color.pink)
-                            .cornerRadius(10)
+                        Text("5 credits").font(.footnote)
                     }
+
                     // PLUS button
-                    Button {
-                        // Background Color Initiate
-                        backgrounds = backgrounds.map { _ in Color.white }
-                        
-                        // Change the images
-                        number = number.map{ _ in
-                            Int.random(in: 0..<symbols.count)
+                    VStack {
+                        Button {
+                            processResults(true)
+                        } label: {
+                            Text("Max Spin")
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .padding(.horizontal, 30)
+                                .background(Color.pink)
+                                .cornerRadius(10)
                         }
-                        
-                        // Check winnings
-                        // middle row
-                        if (number[0] == number[1] && number[1] == number[2]) && (number[3] == number[4] && number[4] == number[5]) && (number[6] == number[7] && number[7] == number[8]) && (number[0] == number[4] && number[4] == number[8]) && (number[2] == number[4] && number[4] == number[6]){
-                            
-                            // Won
-                            credits += betAmount * 50
-                            
-                            // Update backgrounds to green
-                            backgrounds = backgrounds.map{ _ in Color.green }
-                            
-                        } else {
-                            credits -= betAmount * 5
-                        }
-                        
-                    } label: {
-                        Text("Spin PLUS")
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .padding(.horizontal, 30)
-                            .background(Color.pink)
-                            .cornerRadius(10)
+                        Text("25 credits").font(.footnote)
                     }
                 }
                 
                 Spacer()
             }
         }
+    }
+    
+    func processResults(_ isMax:Bool = false) {
+        
+        // Background Color Initiate
+        backgrounds = backgrounds.map { _ in Color.white }
+        
+        if isMax {
+            // Spin all the cards
+            number = number.map { _ in
+                Int.random(in: 0..<symbols.count)
+            }
+        } else {
+            // Spin the middle row
+            // Change the images
+            number = number.enumerated().map {
+                // new range checking operator
+                if 3...5 ~= $0.0 {
+                    return Int.random(in: 0..<symbols.count)
+                } else {
+                    return $0.1
+                }
+            }
+        }
+        // Check winnings
+        processWin(isMax)
+    }
+    
+    func processWin(_ isMax:Bool = false) {
+        // Check winnings
+        
+        var matches = 0
+        
+        if !isMax {
+            // Processing for single spin
+            // middle row
+            if number[3] == number[4] && number[4] == number[5] {
+                
+                // Won
+                matches += 1
+                
+                // Update backgrounds to green
+                backgrounds = backgrounds.enumerated().map{
+                    if 3...5 ~= $0.0 {
+                        return Color.green
+                    } else {
+                        return Color.white
+                    }
+                }
+            }
+        } else {
+            // Processing for max spin
+            // top row
+            if number[0] == number[1] && number[1] == number[2] {
+                
+                // Won
+                matches += 1
+                
+                // Update backgrounds to green
+                backgrounds = backgrounds.enumerated().map{
+                    if 0...2 ~= $0.0 {
+                        return Color.green
+                    } else {
+                        return Color.white
+                    }
+                }
+            }
+            // middle row
+            if number[3] == number[4] && number[4] == number[5] {
+                
+                // Won
+                matches += 1
+                
+                // Update backgrounds to green
+                backgrounds = backgrounds.enumerated().map{
+                    if 3...5 ~= $0.0 {
+                        return Color.green
+                    } else {
+                        return Color.white
+                    }
+                }
+            }
+            // bottom row
+            if number[6] == number[7] && number[7] == number[8] {
+                
+                // Won
+                matches += 1
+                
+                // Update backgrounds to green
+                backgrounds = backgrounds.enumerated().map{
+                    if 6...8 ~= $0.0 {
+                        return Color.green
+                    } else {
+                        return Color.white
+                    }
+                }
+            }
+            
+            // diagonal top left to bottom right
+            if number[0] == number[4] && number[4] == number[8] {
+                
+                // Won
+                matches += 1
+                
+                // Update backgrounds to green
+                backgrounds = backgrounds.enumerated().map{
+                    if [0,4,8].contains($0.0) {
+                        return Color.green
+                    } else {
+                        return Color.white
+                    }
+                }
+            }
+            
+            // diagonal top right to bottom left
+            if number[2] == number[4] && number[4] == number[6] {
+                
+                // Won
+                matches += 1
+                
+                // Update backgrounds to green
+                backgrounds = backgrounds.enumerated().map{
+                    if [2,4,6].contains($0.0) {
+                        return Color.green
+                    } else {
+                        return Color.white
+                    }
+                }
+            }
+        }
+        
+        //Check matches and distributes credits
+        if matches > 0 {
+            // at least one win
+            credits += (matches * betAmount * 2)
+
+        } else if !isMax {
+            // 0 wins, single spin
+            credits -= betAmount
+        } else {
+            // 0 wins, max spin
+            credits -= betAmount * 5
+        }
+        
     }
 }
 
